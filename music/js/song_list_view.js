c2273825b2c3004e40fc95bc106b79d42b1e58c0
Jesse Miner <jesse@givegab.com>
2019-02-26 01:12:14 -0500

    Extract song list view

---
+++ music/js/song_list_view.js
@@ -0,0 +1,36 @@
+SongListView = function () {
+  this.el = document.querySelector('.song-list');
+
+  this.addPlayButtonListener = function () {
+    this.el.addEventListener('click', function (e) {
+      var btn = e.target;
+      if (!btn.classList.contains('play-song')) { return; }
+
+      var songId = parseInt(btn.getAttribute('data-id'));
+      var song = app.songs.find(function (s) {
+        return s.id === songId;
+      });
+      app.player.src = 'https://drive.google.com/uc?export=download&id=' + song.file_id;
+      app.player.play();
+      document.querySelector('.current-song').textContent = song.title;
+    });
+  };
+
+  this.render = function () {
+    var self = this;
+    app.songs.forEach(function (song) {
+      var row = document.createElement('tr');
+      row.classList.add('song-row');
+      row.innerHTML = `
+        <td>
+          <div class="flex-vertical-center">
+            <img class="play-song" data-id="${song.id}" src="images/play.svg">
+            <span class="margin-left">${song.title.substring(0, 40)}</span>
+          </div>
+        </td>
+        <td class="artist-name">${song.artist.substring(0, 40)}</td>
+      `;
+      self.el.appendChild(row);
+    });
+  };
+};
