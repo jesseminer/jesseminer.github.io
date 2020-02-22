395aa0cc912ab5dae37e431ca9c56bc1d1d2254c
Jesse Miner <jesse@givegab.com>
2020-02-22 00:10:59 -0500

    Write slot machine in vue

--- slots/slots.js
+++ slots/slots.js
@@ -1,68 +1,82 @@
-var startMoney = 200;
-var bank;
-var bet;
-var images = ['skull.png', 'cornell.png', 'mario.png', 'blotus.png'];
-var values = [-4, 1, 2, 3];
+new Vue({
+  el: '#container',
+  data: {
+    betOptions: [1, 10, 50, 100, 'all'],
+    images: ['skull.png', 'cornell.png', 'mario.png', 'blotus.png'],
+    message: '',
+    money: 200,
+    redMessage: false,
+    results: [0, 0, 0],
+    rewards: [-4, 1, 2, 3],
+    selectedBet: 10
+  },
 
-function setBank(amt) {
-  bank = amt;
-  $('#bank').html(bank);
-}
+  methods: {
+    betLabel: function (amount) {
+      return amount === 'all' ? 'All I have left!' : amount;
+    },
 
-function setMsg(msg) {
-  $('#message').html(msg);
-}
+    count: function (s1, s2, s3) {
+      var counts = [0, 0, 0, 0];
+      counts[s1]++;
+      counts[s2]++;
+      counts[s3]++;
+      return counts;
+    },
 
-function randomImage() {
-  return Math.floor(Math.random() * 4);
-}
+    getWinnings: function (bet) {
+      var x;
+      var s1 = this.results[0];
+      var s2 = this.results[1];
+      var s3 = this.results[2];
 
-function setSlots(s1, s2, s3) {
-  $('#slot1').attr('src', images[s1]);
-  $('#slot2').attr('src', images[s2]);
-  $('#slot3').attr('src', images[s3]);
-}
+      if (s1 == s2 && s1 == s3) {
+        x = this.rewards[s1] * 9;
+      } else {
+        var c = this.count(s1, s2, s3);
+        var indexOf2 = c.indexOf(2);
+        if (indexOf2 > -1) x = this.rewards[indexOf2] * 4 + this.rewards[c.indexOf(1)];
+        else x = (this.rewards[s1] + this.rewards[s2] + this.rewards[s3]) / 2;
+      }
+      return Math.round(x * bet);
+    },
 
-function start() {
-  setSlots(0, 0, 0);
-  setBank(startMoney);
-  setMsg("");
-  $('#bet2').click();
-}
+    randomImage: function () {
+      return Math.floor(Math.random() * 4);
+    },
 
-function spin() {
-  if (bank <= 0) { setMsg("You're broke!"); return; }
-  if ($('#bet5').prop('checked')) bet = bank;
-  if (bet > bank) { setMsg("Not enough $!"); return; }
-  var s1 = randomImage();
-  var s2 = randomImage();
-  var s3 = randomImage();
-  setSlots(s1, s2, s3);
-  results = getWinnings(s1, s2, s3);
-  setBank(Math.max(0, bank + results[0]));
-  setMsg(results[1]);
-}
+    reset: function () {
+      this.results = [0, 0, 0];
+      this.money = 200;
+      this.selectedBet = 10;
+      this.setMsg('');
+    },
 
-function getWinnings(s1, s2, s3) {
-  var x;
-  if (s1 == s2 && s1 == s3) x = values[s1] * 9;
-  else {
-    var c = count(s1, s2, s3);
-    var indexOf2 = c.indexOf(2);
-    if (indexOf2 > -1) x = values[indexOf2] * 4 + values[c.indexOf(1)];
-    else x = (values[s1] + values[s2] + values[s3]) / 2;
+    setMsg: function (msg, red) {
+      this.message = msg;
+      this.redMessage = red;
+    },
+
+    spin: function () {
+      if (this.money <= 0) {
+        this.setMsg("You're broke!");
+        return;
+      }
+
+      var bet = this.selectedBet === 'all' ? this.money : this.selectedBet;
+
+      if (bet > this.money) {
+        this.setMsg('Not enough $!');
+        return;
+      }
+
+      this.results = [this.randomImage(), this.randomImage(), this.randomImage()];
+      var amountWon = this.getWinnings(bet);
+      this.money = Math.max(0, this.money + amountWon);
+
+      amountWon > 0 ?
+        this.setMsg('+' + amountWon) :
+        this.setMsg(amountWon, true);
+    }
   }
-  var won = Math.round(x * bet);
-  
-  if (won >= 0) msg = "+" + won;
-  else msg = '<span style="color: red;">' + won + '</span>';
-  return [parseInt(won), msg];
-}
-
-function count(s1, s2, s3) {
-  counts = [0, 0, 0, 0];
-  counts[s1]++;
-  counts[s2]++;
-  counts[s3]++;
-  return counts;
-}
+});
