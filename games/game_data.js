207cabcaa57eb24c38d2a50fddf752050d734eda
Jesse Miner <jesse@givegab.com>
2022-06-26 11:45:03 -0400

    Make Forza its own series

--- games/game_data.js
+++ games/game_data.js
@@ -1,7 +1,7 @@
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 series = ["", "Army of Two", "Assassin's Creed", "Battlefield", "Call of Duty", "Crackdown", "The Elder Scrolls",
-          "Fable", "Fallout", "Gears of War", "Halo", "Mass Effect", "Medal of Honor", "BioShock"];
+          "Fable", "Fallout", "Gears of War", "Halo", "Mass Effect", "Medal of Honor", "BioShock", "Forza"];
 
 games = [
   g("Halo: Combat Evolved", "11/15/2001", 10),
@@ -17,7 +17,7 @@ games = [
 
   g("Crackdown", "2/20/2007", 5),
   g("Ghost Recon Advanced Warfighter 2", "3/6/2007", 0),
-  g("Forza Motorsport 2", "5/29/2007", 0),
+  g("Forza Motorsport 2", "5/29/2007", 14),
   g("BioShock", "8/21/2007", 13),
   g("Halo 3", "9/25/2007", 10),
   g("The Orange Box", "10/10/2007", 0),
@@ -35,7 +35,7 @@ games = [
   g("Halo Wars", "3/3/2009", 10),
   g("Halo 3: ODST", "9/22/2009", 10),
   g("Borderlands", "10/20/2009", 0),
-  g("Forza Motorsport 3", "10/27/2009", 0),
+  g("Forza Motorsport 3", "10/27/2009", 14),
   g("CoD: Modern Warfare 2", "11/10/2009", 4),
   g("Assassin's Creed 2", "11/17/2009", 2),
 
@@ -56,7 +56,7 @@ games = [
   g("Crysis 2", "3/22/2011", 0),
   g("Portal 2", "4/19/2011", 0),
   g("Gears of War 3", "9/20/2011", 9),
-  g("Forza Motorsport 4", "10/11/2011", 0),
+  g("Forza Motorsport 4", "10/11/2011", 14),
   g("Battlefield 3", "10/25/2011", 3),
   g("CoD: Modern Warfare 3", "11/8/2011", 4),
   g("The Elder Scrolls V: Skyrim", "11/11/2011", 6),
@@ -65,7 +65,7 @@ games = [
 
   g("Mass Effect 3", "3/6/2012", 11),
   g("Borderlands 2", "9/18/2012", 0),
-  g("Forza Horizon", "10/23/2012", 0),
+  g("Forza Horizon", "10/23/2012", 14),
   g("Medal of Honor Warfighter", "10/23/2012", 12),
   g("Assassin's Creed III", "10/30/2012", 2),
   g("Halo 4", "11/6/2012", 10),
@@ -79,12 +79,12 @@ games = [
   g("Assassin's Creed IV: Black Flag", "10/29/2013", 2),
   g("Battlefield 4", "10/29/2013", 3),
   g("Call of Duty: Ghosts", "11/5/2013", 4),
-  g("Forza Motorsport 5", "11/22/2013", 0),
+  g("Forza Motorsport 5", "11/22/2013", 14),
 
   g("Thief", "2/25/2014", 0),
   g("Titanfall", "3/11/2014", 0),
   g("Destiny", "9/9/2014", 0),
-  g("Forza Horizon 2", "9/30/2014", 0),
+  g("Forza Horizon 2", "9/30/2014", 14),
   g("Call of Duty: Advanced Warfare", "11/4/2014", 4),
   g("Assassin's Creed Unity", "11/11/2014", 2),
   g("Halo: The Master Chief Collection", "11/11/2014", 10),
@@ -93,13 +93,13 @@ games = [
   g("Evolve", "2/10/2015", 0),
   g("Battlefield Hardline", "3/17/2015", 3),
   g("Gears of War: Ultimate Edition", "8/25/2015", 9),
-  g("Forza Motorsport 6", "9/15/2015", 0),
+  g("Forza Motorsport 6", "9/15/2015", 14),
   g("Assassin's Creed Syndicate", "10/23/2015", 2),
   g("Halo 5: Guardians", "10/27/2015", 10),
   g("Call of Duty: Black Ops 3", "11/6/2015", 4),
   g("Fallout 4", "11/10/2015", 8),
 
-  g("Forza Horizon 3", "9/27/2016", 0),
+  g("Forza Horizon 3", "9/27/2016", 14),
   g("Gears of War 4", "10/11/2016", 9),
   g("Battlefield 1", "10/21/2016", 3),
   g("Titanfall 2", "10/28/2016", 0),
@@ -108,11 +108,11 @@ games = [
   g("Halo Wars 2", "2/21/2017", 10),
   g("Mass Effect: Andromeda", "3/21/2017", 11),
   g("Destiny 2", "9/6/2017", 0),
-  g("Forza Motorsport 7", "10/3/2017", 0),
+  g("Forza Motorsport 7", "10/3/2017", 14),
   g("Assassin's Creed Origins", "10/27/2017", 2),
   g("Call of Duty: WWII", "11/3/2017", 4),
 
-  g("Forza Horizon 4", "10/2/2018", 0),
+  g("Forza Horizon 4", "10/2/2018", 14),
   g("Assassin's Creed Odyssey", "10/5/2018", 2),
   g("Call of Duty: Black Ops 4", "10/12/2018", 4),
   g("Red Dead Redemption 2", "10/26/2018", 0),
@@ -129,7 +129,7 @@ games = [
   g("Call of Duty: Black Ops Cold War", "11/13/2020", 4),
 
   g("Call of Duty: Vanguard", "11/5/2021", 4),
-  g("Forza Horizon 5", "11/9/2021", 0),
+  g("Forza Horizon 5", "11/9/2021", 14),
   g("Battlefield 2042", "11/19/2021", 3),
   g("Halo Infinite", "12/8/2021", 10)
 ];
